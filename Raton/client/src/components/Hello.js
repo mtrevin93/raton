1c6f93b884f83f9a9cd3dba429d1e14f0b3e825d
Michael Trevino <mtrevin93@gmail.com>
2021-10-22 07:09:26 -0400

    formatting for text display page

--- Raton/client/src/components/Hello.js
+++ Raton/client/src/components/Hello.js
@@ -4,14 +4,23 @@ import { Container } from "@mui/material";
 export default function Hello() {
 
 const triggerClick = () => {
-  debugger
+  if (checkedState === true)
+  {
+    checkedState = false;
+  }
+  else if (checkedState === false)
+  {
+    checkedState = true;
+  }
   console.log("hi")
 }
 
+let checkedState = true;
+
 const htmlTest = ['<p class="lan1>', 
 "https://uploads-ssl.webflow.com/581a8b6ad92743753594a969/5bce906d357ce26602b4f616_chicken2-sp.jpg",
-"Está leyendo las noticias. Ve un artículo aterrador con un título aterrador. Dice: ¡EL CIELO SE ESTÁ CAYENDO!<br/>",
-'</p>']
+"Está ", "leyendo ", "las ", "noticias ", "Ve ", "un ", "artículo ", "aterrador ", "con ", "un ", "título ", "aterrador ", "Dice ", "EL ", "CIELO ", "SE ", "ESTÁ ", "CAYENDO ",
+'</p>', '<p class="lan1>', 'esta ', 'blah ', 'blah ']
 
 const words = []
 
@@ -34,7 +43,7 @@ const words = []
       }
     })}
       {words?.map((w) => {
-        return <><label htmlFor={w} style={{ fontSize: 30 }}>{w}</label><input type="checkbox" name={w} onClick={() => triggerClick()}></input></>
+        return <><label htmlFor={w} onClick={() => triggerClick()} style={{ fontSize: 30 }}>{w}</label><input style={{display: "none"}} type="checkbox" checked={checkedState ===true? "checked" : false} name={w}></input></>
       })}
   
   </Container>
